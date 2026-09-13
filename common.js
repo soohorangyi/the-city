8073dadbe08f43cb7a0f157f80bde1513f019d66
Shin SooYi <5the4bewithus03@gmail.com>
2026-09-13 18:58:19 +0900

    Update common.js

--- common.js
+++ common.js
@@ -78,7 +78,7 @@ function bnRenderFooter() {
   var el = document.getElementById("bn-footer");
   if (!el) return;
   el.innerHTML =
-    '<p>The Belle City Herald-Tribune &middot; 격주 발행 &middot; 제보 및 편지는 편집부로</p>';
+    '<p>The Belle City Herald-Tribune<br class="bn-mobile-br"/>격주 발행 &middot; 제보 및 편지는 편집부로</p>';
 }
 
 document.addEventListener("DOMContentLoaded", function () {
